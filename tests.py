c6f5c405f5bd8086cf184bfe2f33f089b46432e2
CheemsEnjoyer <95558369+CheemsEnjoyer@users.noreply.github.com>
2025-09-25 16:19:46 +0800

    a

--- tests.py
+++ tests.py
@@ -12,7 +12,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(subtract(0, 5), -5)
 
     def test_multiply(self):
-        self.assertEqual(multiply(3, 4), 11)
+        self.assertEqual(multiply(3, 4), 12)
         self.assertEqual(multiply(-2, 3), -6)
 
     def test_divide(self):
